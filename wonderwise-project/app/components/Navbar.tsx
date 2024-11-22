4c9c72e0176587519d9a62388a90c8067daa5549
ALbert-Sezkir <a_sezkir@hotmail.com>
2024-11-22 17:54:01 +0100

    added hamburger icon on sm devices

--- wonderwise-project/app/components/Navbar.tsx
+++ wonderwise-project/app/components/Navbar.tsx
@@ -6,15 +6,21 @@ import Link from "next/link";
 import { useRouter } from "next/navigation";
 import { useAuth } from "../context/AuthContext";
 import { signOut } from "firebase/auth";
-import { auth } from "../../firebaseConfig"; // Import your Firebase config
+import { auth } from "../../firebaseConfig"; 
+import { FaBars } from 'react-icons/fa'; 
 
 const Navbar = () => {
   const { user } = useAuth();
   const [isOpen, setIsOpen] = useState(false);
+  const [isMobileMenuOpen, setIsMobileMenuOpen] = useState(false);
   const router = useRouter();
 
   const toggleMenu = () => {
     setIsOpen(!isOpen);
+  };
+
+  const toggleMobileMenu = () => {
+    setIsMobileMenuOpen(!isMobileMenuOpen);
   };
 
   const handleLogout = async () => {
@@ -47,7 +53,7 @@ const Navbar = () => {
             className="object-contain"
           />
         </Link>
-        <div className="relative">
+        <div className="relative hidden lg:block">
           <button onClick={toggleMenu} className="focus:outline-none">
             <div className="w-16 h-16 rounded-full overflow-hidden">
               <Image
@@ -61,6 +67,45 @@ const Navbar = () => {
           </button>
           {isOpen && (
             <div className="absolute right-1/2 transform translate-x-1/2 mt-2 w-48 bg-white border rounded-lg shadow-lg z-50">
+              {user ? (
+                <>
+                  <Link href="/profile">
+                    <div className="block px-4 py-2 text-gray-800 font-livvic hover:bg-gray-200">Profile</div>
+                  </Link>
+
+                  <Link href="/my-reservations">
+                    <div className="block px-4 py-2 text-gray-800 font-livvic hover:bg-gray-200">My Reservations</div>
+                  </Link>
+
+                  <Link href="/admin">
+                    <div className="block px-4 py-2 text-gray-800 font-livvic hover:bg-gray-200">My WonderWise</div>
+                  </Link>
+                  <div
+                    onClick={handleLogout}
+                    className="block px-4 py-2 text-gray-800 font-livvic hover:bg-gray-200 cursor-pointer"
+                  >
+                    Logout
+                  </div>
+                </>
+              ) : (
+                <>
+                  <Link href="/login">
+                    <div className="block px-4 py-2 text-gray-800 font-livvic hover:bg-gray-200">Log in</div>
+                  </Link>
+                  <Link href="/register">
+                    <div className="block px-4 py-2 text-gray-800 font-livvic hover:bg-gray-200">Register</div>
+                  </Link>
+                </>
+              )}
+            </div>
+          )}
+        </div>
+        <div className="lg:hidden">
+          <button onClick={toggleMobileMenu} className="focus:outline-none">
+            <FaBars size={24} />
+          </button>
+          {isMobileMenuOpen && (
+            <div className="absolute right-0 mt-2 w-48 bg-white border rounded-lg shadow-lg z-50">
               {user ? (
                 <>
                   <Link href="/profile">
